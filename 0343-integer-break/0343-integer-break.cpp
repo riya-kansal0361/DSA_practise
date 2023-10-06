24a87b39f4afee7f103d55843ade6e86a9796009
riya-kansal0361 <130249605+riya-kansal0361@users.noreply.github.com>
2023-10-07 00:57:53 +0530

    Time: 2 ms (49.96%), Space: 6.2 MB (53.64%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-     int t[59];
-    int solve(int n ){
-        
-        if(n == 1)
-            return 1;
-        
-        if(t[n] != -1)
-            return t[n];
-        
+    int t[59];
+    int solve(int n){
+        if(n==1) return 1;
+        if(t[n] != -1) return t[n];
         int result = INT_MIN;
-      
-        for(int i = 1; i<n; i++) {
+        for(int i=1;i<=n-1;i++){
             int prod = i * max(n-i, solve(n-i));
-            
             result = max(result, prod);
         }
         
         return t[n] = result;
-        
     }
-    
     int integerBreak(int n) {
-        memset(t, -1, sizeof(t));
+        memset(t,-1,sizeof(t));
         return solve(n);
     }
 };
