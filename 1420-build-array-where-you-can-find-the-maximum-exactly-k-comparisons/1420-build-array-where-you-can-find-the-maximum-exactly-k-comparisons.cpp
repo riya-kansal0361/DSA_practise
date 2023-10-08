c485baf6ea162fc08372b7b6904cc8416241e888
riya-kansal0361 <130249605+riya-kansal0361@users.noreply.github.com>
2023-10-08 20:30:35 +0530

    Time: 85 ms (45.91%), Space: 7.4 MB (65.00%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
-     int N, M, K;
-    int MOD = 1e9+7;
+    int N,M,K;
+    int mod = 1e9+7;
     int t[51][51][101];
-    
-    int solve(int idx, int searchCost, int maxSoFar) {
-        if(idx == N) {
-            if(searchCost == K)
-                return 1;
+    int solve(int idx, int search_cost, int max_so_far){
+        if(idx == N){
+            if(search_cost == K) return 1;
+            
             return 0;
         }
-        
-        if(t[idx][searchCost][maxSoFar] != -1) {
-            return t[idx][searchCost][maxSoFar];
-        }
-        
+        if(t[idx][search_cost][max_so_far] != -1) return t[idx][search_cost][max_so_far];
         int result = 0;
-        
-        for(int i = 1; i <= M; i++) {
-            
-            if(i > maxSoFar) {
-                result = (result + solve(idx+1, searchCost+1, i)) % MOD;
-            } else {
-                result = (result + solve(idx+1, searchCost, maxSoFar)) % MOD;
+        for(int i=1;i<=M;i++){
+            if(i>max_so_far){
+                result = (result + solve(idx+1, search_cost + 1, i))%mod;
             }
-            
+            else{
+                result = (result + solve(idx+1,search_cost,max_so_far))%mod;
+            }
         }
-        
-        return t[idx][searchCost][maxSoFar] = result % MOD;
-        
+        return t[idx][search_cost][max_so_far] = result%mod;
     }
-    
     int numOfArrays(int n, int m, int k) {
-        N = n;
-        M = m;
-        K = k;
-        memset(t, -1, sizeof(t));
-        return solve(0, 0, 0);
-        
+        N=n;
+        M=m;
+        K=k;
+        memset(t,-1,sizeof(t));
+        return solve(0,0,0);
     }
 };
