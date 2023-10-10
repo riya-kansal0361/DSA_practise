0d24179d4f320dd8b19a597f1d9e6b739fc895b2
riya-kansal0361 <130249605+riya-kansal0361@users.noreply.github.com>
2023-10-11 01:09:22 +0530

    Time: 424 ms (8.53%), Space: 106.8 MB (28.36%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-           int n = nums.size();
+        int n = nums.size();
         
-        set<int> st(begin(nums), end(nums));
-        
-        vector<int> temp(begin(st), end(st));
-        
+        set<int> st(nums.begin(),nums.end());
+        vector<int> temp(st.begin(), st.end());
         int result = INT_MAX;
-        
-        for(int i = 0; i<temp.size(); i++) {
+        for(int i=0;i<temp.size();i++){
+            int l = temp[i];
+            int r = l + n-1;
             
-            int left_range  = temp[i];
-            int right_range = left_range + n - 1;
+            int j = upper_bound(temp.begin(), temp.end(),r) - temp.begin();
             
-            int j = upper_bound(begin(temp), end(temp), right_range) - begin(temp);
+            int within_range = j-i;
+            int out_range = n - within_range;
             
-            int in_range  = j - i;
-            int out_range = n - in_range;
-            
-            result = min(result, out_range);
-            
+            result = min(result,out_range);
         }
-        
         return result;
     }
 };
