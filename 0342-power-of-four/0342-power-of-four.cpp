5294ffe365078b4c7cc9527b7d2e21d47a52507c
riya-kansal0361 <130249605+riya-kansal0361@users.noreply.github.com>
2023-10-24 00:08:17 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (62.77%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -9,9 +9,13 @@ public:
         
 //         return false;
          if(n <= 0) return false;
-        int x = log(n)/log(4);
+//         int x = log(n)/log(4);
         
-        if(n == pow(4,x)) return true;
+//         if(n == pow(4,x)) return true;
+        
+//         return false;
+        
+        if((n&(n-1)) == 0 && (n-1)%3 == 0) return true;
         
         return false;
     }
