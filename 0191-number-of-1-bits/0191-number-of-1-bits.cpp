0194d140e385f18991d9b3d61d754156281d5963
riya-kansal0361 <130249605+riya-kansal0361@users.noreply.github.com>
2023-12-04 17:08:00 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (58.31%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        
+        for(int i=31;i>=0;i--){
+            if(((n>>i) & 1) == 1){
+                count++;
+            }
+        }
+        return count;
+    }
+};
