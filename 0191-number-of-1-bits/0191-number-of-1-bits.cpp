c1279f08f3982e799b951eb40b4e0568d1952cc0
riya-kansal0361 <130249605+riya-kansal0361@users.noreply.github.com>
2023-12-04 17:08:32 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (58.31%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,10 +3,15 @@ public:
     int hammingWeight(uint32_t n) {
         int count = 0;
         
-        for(int i=31;i>=0;i--){
-            if(((n>>i) & 1) == 1){
-                count++;
-            }
+        // for(int i=31;i>=0;i--){
+        //     if(((n>>i) & 1) == 1){
+        //         count++;
+        //     }
+        // }
+        
+        while(n){
+            n = (n & (n-1));
+            count++;
         }
         return count;
     }
