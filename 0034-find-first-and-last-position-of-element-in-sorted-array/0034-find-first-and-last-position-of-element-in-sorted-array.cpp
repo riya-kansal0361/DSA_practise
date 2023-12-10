d1f8de10dec7727a0c8a478aec838ace1f691c5a
riya-kansal0361 <130249605+riya-kansal0361@users.noreply.github.com>
2023-12-11 02:46:02 +0530

    Time: 0 ms (100.00%), Space: 14 MB (61.50%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,48 +1,14 @@
+
+#include<algorithm>
 class Solution {
 public:
-    int find_left(vector<int>& arr, int n, int target){
-        int left = -1;
-        
-        int l=0,r=n-1;
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            if(arr[mid] == target){
-                left = mid;
-                r = mid-1;
-            }
-            else if(arr[mid] > target){
-                r = mid-1;
-            }
-            else{
-                l = mid+1;
-            }
-        }
-        return left;
-    }
-    int find_right(vector<int>& arr, int n, int target){
-        int right = -1;
-        
-        int l=0,r=n-1;
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            if(arr[mid] == target){
-                right = mid;
-                l = mid+1;
-            }
-            else if(arr[mid] > target){
-                r = mid-1;
-            }
-            else{
-                l = mid+1;
-            }
-        }
-        return right;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        int l = find_left(nums,n,target);
-        int r = find_right(nums,n,target);
+        int lb = lower_bound(nums.begin(), nums.end(),target) - nums.begin();
         
-        return {l,r};
+        if(lb == nums.size() || nums[lb] != target){
+            return {-1,-1};
+        }
+        int ub = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
+        return {lb, ub-1};
     }
 };
