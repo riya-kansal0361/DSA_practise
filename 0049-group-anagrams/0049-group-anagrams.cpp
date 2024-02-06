ef0d76ed01ae6622350836986f8354545dd0c6b5
riya-kansal0361 <130249605+riya-kansal0361@users.noreply.github.com>
2024-02-06 22:55:19 +0530

    Time: 35 ms (38.55%), Space: 24.5 MB (20.94%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
+    string generate(string &word){
+        int arr[26] = {0};
+        
+        for(int i=0;i<word.size();i++){
+            arr[word[i] - 'a']++;
+        }
+        string ans = "";
+        
+        for(int i=0;i<26;i++){
+            int freq = arr[i];
+            
+            ans += string(freq, i+'a');
+        }
+       return ans;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans;
         map<string,vector<string>> mp;
+        
+        
         for(auto &it : strs){
             string s = it;
-            sort(it.begin(), it.end());
-            string t = it;
             
-                mp[t].push_back(s);
-        
+            string new_word = generate(s);
+            
+            mp[new_word].push_back(s);
         }
         
         for(auto &it : mp){
-            vector<string> s;
-            for(auto &i : it.second){
-                s.push_back(i);
-            }
-            ans.push_back(s);
+            ans.push_back(it.second);
         }
-        
         return ans;
     }
 };
