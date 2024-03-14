623bda3f7204824f26781af0c475b0ba3f3761f6
riya-kansal0361 <130249605+riya-kansal0361@users.noreply.github.com>
2024-03-14 20:25:05 +0530

    Time: 18 ms (90.15%), Space: 31.2 MB (66.62%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -3,19 +3,47 @@ public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         int n = nums.size();
         
-        unordered_map<int,int> mp;
+//         unordered_map<int,int> mp;
         
-        int result = 0;
-        int sum = 0;
-        mp[sum]++;
-        for(int i=0;i<n;i++){
-            sum += nums[i];
-            int cum = sum - goal;
+//         int result = 0;
+//         int sum = 0;
+//         mp[sum]++;
+//         for(int i=0;i<n;i++){
+//             sum += nums[i];
+//             int cum = sum - goal;
             
-            if(mp[cum]){
-                result += mp[cum];
+//             if(mp[cum]){
+//                 result += mp[cum];
+//             }
+//             mp[sum]++;
+//         }
+        
+//         return result;
+        
+        //SLIDING WINDOW
+        
+        int i=0;
+        int j=0;
+        
+        int sum = 0;
+        int result = 0;
+        int zero = 0;
+        
+        while(j<n){
+            sum += nums[j];
+            
+            while(i<j && (nums[i] ==0 || sum > goal)){
+                if(nums[i] == 0) zero++;
+                else zero=0;
+                
+                sum -= nums[i];
+                i++;
             }
-            mp[sum]++;
+            
+            if(sum == goal){
+                result += 1 + zero;
+            }
+            j++;
         }
         
         return result;
