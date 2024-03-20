d204659d474dc500f576fec90314e052fbd40794
riya-kansal0361 <130249605+riya-kansal0361@users.noreply.github.com>
2024-03-21 03:11:03 +0530

    Time: 44 ms (83.15%), Space: 37.9 MB (90.50%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,47 +1,25 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
+        
         vector<int> mp(26,0);
         
         for(char &ch : tasks){
             mp[ch-'A']++;
         }
+        sort(mp.begin(),mp.end());
+        int maxFreq = mp[25];
+        int gadhe =maxFreq-1;
+        int idle = n*gadhe;
         
-        priority_queue<int> pq;
-        
-        for(int i=0;i<26;i++){
-            if(mp[i]>0){
-                pq.push(mp[i]);
-            }
+        for(int i=24;i>=0;i--){
+            
+            idle -= min(mp[i],gadhe);
         }
         
-        int time = 0;
+        if(idle>0) return tasks.size()+idle;
         
-        while(!pq.empty()){
-            vector<int> temp;
-            
-            
-            for(int i=1;i<=n+1;i++){
-                if(!pq.empty()){
-                int freq = pq.top();
-                pq.pop();
-                freq--;
-                temp.push_back(freq);
-            }
-            }
-            
-            for(int &f : temp){
-                if(f>0) pq.push(f);
-            }
-            
-            if(pq.empty()){
-                time += temp.size();
-            }
-            else{
-                time += (n+1);
-            }
-        }
         
-        return time;
+        return tasks.size();
     }
 };
